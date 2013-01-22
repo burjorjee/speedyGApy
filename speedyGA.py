607862ab7afaf6392ded08f7bf15f71242e629bd
Keki Burjorjee <keki@zite.com>
2013-01-23 03:34:59 +1100

    Improved extensibility

--- speedyGA.py
+++ speedyGA.py
@@ -1,28 +1,32 @@
+from functools import partial
 from random import random
 from matplotlib.pyplot import figure, plot, xlabel, ylabel, title, axis, hold
 from numpy import *
 from numpy.random import rand, randn
-from multiprocessing import Pool
 
-def evaluate(args):
-    pop, pivLoci, index = args
-    return remainder(pop[index, pivLoci].sum(),2)*0.5-.25+random.random()
+def visualizeGen(pop, gen, avgFitness, maxFitness):
+    popSize,length = pop.shape
+    f = figure(1)
+    hold(False)
+    bitFreqs = pop.sum(axis=0).astype('float')/popSize
+    plot(arange(length), bitFreqs,'b.', markersize=2)
+    axis([0, length, 0, 1])
+    title("Generation = %s, Average Fitness = %0.3f " % (gen,avgFitness))
+    ylabel('Frequency of the Bit 1')
+    xlabel('Locus')
+    f.canvas.draw()
+    f.show()
 
-def main():
-    length = 1000
-    popSize = 500
-    maxGens = 500
-    probCrossover = 1
-    probMutation = 0.005
-    sigmaScaling = True
-    sigmaScalingCoeff = 1
-    SUS = True
-    visualize = True
-    verbose = True
-    parallelFitnessEvaluation = False
-
-    if parallelFitnessEvaluation:
-        pool = Pool(processes=5)
+def evolve(fitnessFunction,
+            length,
+            popSize=500,
+            maxGens=500,
+            probCrossover=1,
+            probMutation=0.005,
+            sigmaScaling=True,
+            sigmaScalingCoeff=1,
+            SUS=True,
+            visualizeGen=visualizeGen):
 
     maskReposFactor = 5
     uniformCrossoverMaskRepos = rand(popSize/2, (length+1)*maskReposFactor) < 0.5
@@ -31,45 +35,19 @@ def main():
     avgFitnessHist = zeros(maxGens+1)
     maxFitnessHist = zeros(maxGens+1)
 
-
-
-    pivLoci = floor(rand(4)*length).astype('int16')
-
-    otherLoci = floor(rand(15)*length).astype('int16')
-
-    #pop = rand(popSize, length)<0.5
     pop = zeros((popSize, length), dtype='int8')
     pop[rand(popSize, length)<0.5] = 1
 
     f = figure(1)
-    #f.set_facecolor('white')
     for gen in xrange(maxGens):
-        #arglist = [[pop, pivLoci, i] for i in range(popSize)]
-        if parallelFitnessEvaluation:
-            fitnessVals = pool.map(evaluate, arglist)
-        else:
-            #fitnessVals = [evaluate(args) for args in arglist]
-            fitnessVals = remainder(pop[:, pivLoci].sum(axis=1),2)*0.5-.25+randn(popSize)
-        fitnessVals = transpose(fitnessVals)
+
+        fitnessVals = transpose(fitnessFunction(pop))
         maxFitnessHist[gen] = max(fitnessVals)
         avgFitnessHist[gen] = mean(fitnessVals)
 
         print "gen = %.3d   avgFitness = %3.3f  maxfitness = %3.3f" % (gen, avgFitnessHist[gen], maxFitnessHist[gen])
-        if visualize:
-            figure(1)
-            hold(False)
-            bitFreqs = pop.sum(axis=0).astype('float')/popSize
-            plot(arange(length), bitFreqs,'b.', markersize=2)
-            hold(True)
-            plot(pivLoci, bitFreqs[pivLoci], 'r.', markersize=15)
-            #plot(otherLoci, bitFreqs[otherLoci], 'g.', markersize= 15)
-            axis([0, length, 0, 1])
-            title("Generation = %s, Average Fitness = %0.3f " % (gen,avgFitnessHist[gen]))
-            ylabel('Frequency of the Bit 1')
-            xlabel('Locus')
-            f.canvas.draw()
-            f.show()
-
+        if visualizeGen:
+            visualizeGen(pop, gen=gen, avgFitness=avgFitnessHist[gen], maxFitness=maxFitnessHist[gen])
         if sigmaScaling:
             sigma = std(fitnessVals)
             if sigma:
@@ -113,13 +91,38 @@ def main():
         pop[masks] = pop[masks] + 1
         pop = remainder(pop, 2)
 
-    if verbose:
-        figure(2)
-        hold(False)
-        plot(arange(maxGens+1), avgFitnessHist,'k-')
-        hold(True)
-        plot(arange(maxGens+1), avgFitnessHist, 'c-')
-        xlabel('Generation')
-        ylabel('Fitness')
+    f=figure(2)
+    hold(False)
+    plot(arange(maxGens+1), avgFitnessHist,'k-')
+    hold(True)
+    plot(arange(maxGens+1), maxFitnessHist, 'c-')
+    xlabel('Generation')
+    ylabel('Fitness')
+    f.show()
 
-main()
+def stochasticEffectiveAttributeParity(pop, pivLoci):
+    return remainder(pop[:, pivLoci].sum(axis=1),2)*0.5-.25+randn(len(pop))
+
+def seapEvolve():
+    length = 1000
+    pivLoci = floor(rand(4)*length).astype('int16')
+    evolve(partial(stochasticEffectiveAttributeParity,pivLoci=pivLoci), length,
+        visualizeGen=partial(seapVisualizeGen,pivLoci=pivLoci))
+
+def seapVisualizeGen(pop, gen, avgFitness, maxFitness, pivLoci):
+    popSize,length = pop.shape
+    f = figure(1)
+    hold(False)
+    bitFreqs = pop.sum(axis=0).astype('float')/popSize
+    plot(arange(length), bitFreqs,'b.', markersize=2)
+    hold(True)
+    plot(pivLoci, bitFreqs[pivLoci], 'r.', markersize=15)
+    axis([0, length, 0, 1])
+    title("Generation = %s, Average Fitness = %0.3f " % (gen,avgFitness))
+    ylabel('Frequency of the Bit 1')
+    xlabel('Locus')
+    f.canvas.draw()
+    f.show()
+
+if __name__=="__main__":
+    seapEvolve()
